ccf9e2f44ef03ab2f700da035d6b9b597ecfeedc
dm.sviridov <dm@svrdv.ru>
2024-03-16 19:44:02 +0300

    some fixes

--- js/game.js
+++ js/game.js
@@ -1,15 +1,24 @@
 function onButtonPush() {
     let button = document.getElementById("strangeButton")
-    if(button.textContent == "thanks") {
+    if (stage == 1) {
+        button.textContent = "Thanks"
+        button.classList = "btn btn-success"
+        stage++
+
+    } else if(stage == 2) {
         button.textContent = "No more"
-    } else if (button.textContent == "No more") {
+        button.classList = "btn btn-warning"
+        stage++
+    } else if (stage == 3) {
         button.textContent = "Stop please"
-    } else if (button.textContent == "Stop please") {
+        button.classList = "btn btn-danger"
+        stage++
+    } else if (stage == 4) {
         button.textContent = "х_х"
         button.classList = "btn btn-secondary"
         button.disabled = true
-    } else {
-        button.textContent = "thanks"
+        stage++
     }
 }
 
+let stage = 1
