b66b43574cb4a385f0fd45c20d9b0d2c2a4601cc
dm.sviridov <dm@svrdv.ru>
2024-03-16 13:32:46 +0300

    added strange button

--- js/init.js
+++ js/init.js
@@ -1,1 +1,15 @@
-alert("Hello!")
+function onButtonPush() {
+    let button = document.getElementById("strangeButton")
+    if(button.textContent == "thanks") {
+        button.textContent = "No more"
+    } else if (button.textContent == "No more") {
+        button.textContent = "Stop please"
+    } else if (button.textContent == "Stop please") {
+        button.textContent = "dead button"
+        button.classList = "btn btn-secondary"
+        button.disabled = true
+    } else {
+        button.textContent = "thanks"
+    }
+}
+
